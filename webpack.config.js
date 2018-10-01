5793122b0cc774eecbaa97ecdb95ea5a8eb360f2
Christian Bär <teggno@hotmail.com>
2018-09-30 21:56:45 -1000

    Added webpack dev server stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,31 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/
       }
     ]
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
+    extensions: [".tsx", ".ts", ".js"]
   },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 8080,
+    host: "127.0.0.1",
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
   }
 };
