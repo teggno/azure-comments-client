afbd1d1821bc2ef94000f151c1a846aee4ad8f95
Christian Bär <teggno@hotmail.com>
2018-09-30 21:57:20 -1000

    Added basic list view of comments, displaying author name only

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,69 @@ import "whatwg-fetch";
 
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("commentContainer");
-  if (container) container.appendChild(getCommentUI("post2"));
+  if (container) {
+    container.appendChild(getCommentUI("post2"));
+    container.appendChild(getCommentListUIWithButton("post2"));
+  }
   // commentsForPost("post2").then(comments => {
   //   const container = document.getElementById("commentContainer");
   //   if (container) container.appendChild(getCommentTestUI(comments));
   // });
 });
+
+function getCommentListUIWithButton(postUrl: string) {
+  var div = document.createElement("div");
+  var showCommentsButton = document.createElement("button");
+  showCommentsButton.innerHTML = "Show comments";
+
+  div.appendChild(showCommentsButton);
+
+  var isListVisible: boolean | null = null;
+  var list: HTMLDivElement;
+  showCommentsButton.addEventListener("click", () => {
+    if (isListVisible === null) {
+      // list has never ever been displayed
+      list = document.createElement("div");
+      var refreshButton = document.createElement("button");
+      refreshButton.innerHTML = "Refresh comments";
+      var ul: HTMLElement;
+      refreshButton.addEventListener("click", () => {
+        commentsForPost(postUrl).then(comments => {
+          list.removeChild(ul);
+          ul = getCommentListUI(comments);
+          list.appendChild(ul);
+        });  
+      });
+      commentsForPost(postUrl).then(comments => {
+        list.appendChild(refreshButton);
+        ul = getCommentListUI(comments);
+        list.appendChild(ul);
+        showCommentsButton.innerHTML = "Hide comments";
+        isListVisible = true;
+        div.appendChild(list);
+      });
+    } else if (isListVisible === true) {
+      list.style.display = "none";
+      showCommentsButton.innerHTML = "ShowComments";
+      isListVisible = false;
+    } else {
+      list.style.display = "block";
+      showCommentsButton.innerHTML = "Hide comments";
+      isListVisible = true;
+    }
+  });
+  return div;
+}
+
+function getCommentListUI(comments: Comment[]) {
+  var ul = document.createElement("ul");
+  comments.forEach(comment => {
+    var li = document.createElement("li");
+    li.innerHTML = comment.authorName;
+    ul.appendChild(li);
+  });
+  return ul;
+}
 
 function getCommentUI(postUrl: string) {
   var div = document.createElement("div");
@@ -21,7 +78,7 @@ function getCommentUI(postUrl: string) {
   var textArea: HTMLTextAreaElement;
   leaveACommentButton.addEventListener("click", () => {
     if (isFormVisible === null) {
-      // form has never ever been displayed for
+      // form has never ever been displayed
       form = document.createElement("form");
       form.addEventListener("submit", e => e.preventDefault());
 
@@ -53,8 +110,11 @@ function getCommentUI(postUrl: string) {
           })
           .then(function(token: string) {
             comment.captchaToken = token;
-            return saveComment(comment)
-              .then(() => grecaptcha.reset());
+            return saveComment(comment).then(() => {
+              grecaptcha.reset();
+              textArea.value = "";
+              authorNameTextBox.value = "";
+            });
           });
       });
 
@@ -116,14 +176,14 @@ function commentsForPost(postUrl: string) {
 }
 
 function getSettings() {
-  var azureUrl = "https://adw1blogcomments.azurewebsites.net/api";
+  //var azureUrl = "https://adw1blogcomments.azurewebsites.net/api";
+  var azureUrl = "http://localhost:7071/api";
   return {
     getCommentsUrl: (postUrl: string) =>
       `${azureUrl}/GetComments?postUrl=${encodeURIComponent(postUrl)}`,
     newCommentUrl: () => `${azureUrl}/NewComment`
   };
 }
-
 
 interface CommentBase {
   authorName: string;
