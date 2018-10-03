3379740be6f83834208fc72e7126a2aca4f382be
Christian Bär <teggno@hotmail.com>
2018-10-02 22:18:33 -1000

    Fixed bugs in htmlBuilder and added some descriptions to its functions.

--- src/htmlBuilder.ts
+++ src/htmlBuilder.ts
@@ -1,3 +1,4 @@
+/** Creates a builder for an element of the type indicated by tagName. */
 export default function<K extends keyof HTMLElementTagNameMap>(
   tagName: K,
   createCallback?: (element: HTMLElementTagNameMap[K]) => void
@@ -7,30 +8,49 @@ export default function<K extends keyof HTMLElementTagNameMap>(
     [name: string]: (element: HTMLElementTagNameMap[K]) => void;
   } = {};
   var builder = {
+    /**
+     * Only call once. Otherwise last call (before call to build()) wins.
+     */
     innerHTML: (innerHTML: string) => {
       buildFunctions["innerHTML"] = element => {
         element.innerHTML = innerHTML;
       };
       return builder;
     },
+    /**
+     * Can be called multiple times.
+     */
     attribute: (name: string, value: string) => {
+      var existingFunction = buildFunctions["attribute"];
       buildFunctions["attribute"] = element => {
+        if (existingFunction) existingFunction(element);
         element.setAttribute(name, value);
       };
       return builder;
     },
+    /**
+     * Can be called multiple times.
+     */
     withChildren: (children: (ElementBuilder | ChildBuilder)[]) => {
       children.forEach(c => {
         if (childBuilders.indexOf(c) === -1) childBuilders.push(c);
       });
       return builder;
     },
+    /**
+     * Can be called multiple times.
+     */
     addEventListener: (event: string, listener: (e: Event) => void) => {
+      var existingFunction = buildFunctions["addEventListener"];
       buildFunctions["addEventListener"] = element => {
+        if (existingFunction) existingFunction(element);
         element.addEventListener(event, listener);
       };
       return builder;
     },
+    /**
+     * Can be called multiple times.
+     */
     build: () => {
       var element = document.createElement(tagName);
       for (var name in buildFunctions) {
